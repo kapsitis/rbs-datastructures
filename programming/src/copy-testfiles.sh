edce6d84d9c5a6c56972be73047b643713e5c79b
kapsitis <kalvis.apsitis@gmail.com>
2021-09-25 17:05:08 +0300

    Testfiles 01-05

---
+++ programming/src/copy-testfiles.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cp run-lab1-tests.sh $1/
+
+cp lab1-1/Makefile $1/lab1-1/
+cp lab1-1/test*.txt $1/lab1-1/
+cp lab1-1/expected*.txt $1/lab1-1/
+
+cp lab1-2/Makefile $1/lab1-2/
+cp lab1-2/test*.txt $1/lab1-2/
+cp lab1-2/expected*.txt $1/lab1-2/
+
--- programming/src/run-lab1-tests.sh
+++ programming/src/run-lab1-tests.sh
@@ -1,102 +1,94 @@
 #!/bin/bash
-#must have sudo rights
 
-#echo "Pulling from git..."
+#echo "Pull the submitted files from GitHub"
+#Better do this command manually in the place you want - authentication is easier.
 #git clone git@github.com:username/ds-workspace-username.git
 
-declare -a tsts=("01" "02" "03" "04" "05")
+# There are five CPP files in lab1-1: sample1.cpp, ... sample5.cpp (plus sample6.cpp in another directory).
+declare -a programs=( "1" "2" "3" "4" "5" )
+
+# Each CPP file has five testcases (tests with their expected results)
+declare -a tsts=( "01" "02" "03" "04" "05" )
+
+# These are the source files we expect to see:
+declare -a infiles=( "lab1-1/sample1.cpp" "lab1-1/sample2.cpp" "lab1-1/sample3.cpp" "lab1-1/sample4.cpp" "lab1-1/sample5.cpp" "lab1-2/sample6.cpp" )
+
+echo "Checking for missing source files..." 
+# Boolean flag, if there were any missing *.cpp files
+missingFiles=0
+for i in "${infiles[@]}"
+do 
+  if [ ! -f ${i} ]; then
+    echo "ERROR: File ${i} does not exist" 
+    missingFiles=1
+  fi
+done
+if [[ "$missingFiles" -eq "0" ]]; then
+  echo "Check finished. No missing files"
+else 
+  echo "Check finished. Please copy the missing files in their locations."
+  echo "The grading script may lead to unpredictable results, if required files are missing."
+fi 
+
+
+#######################################################
+## Run tests in lab1-1 directory.                    ##
+#######################################################
+
+# Remove GRADES file (will append to it later on)
+rm -f GRADES.txt
 
 cd lab1-1
 make clean
 
-
-echo "-----------------------------"
-echo "******** sample1.cpp ********"
-echo "-----------------------------"
-echo "Compiling..."
-make sample1
-FILE=sample1
-if test -f "$FILE"; then
-  for i in "${tsts[@]}"
+for tt in "${programs[@]}" 
+do 
+  echo "-----------------------------"
+  echo "******** sample${tt}.cpp ********"
+  echo "-----------------------------"
+  echo "Compiling..."
+  make "sample${tt}"
+  # If executable exists:
+  if [ -f "sample${tt}" ]
+  then
+    for i in "${tsts[@]}"
     do
-      echo "----- test1.${i}.txt -----"
-      ./sample1 < test1.${i}.txt > test1.${i}.out.txt
-      diff -i -w -B test1.${i}.out.txt expected1.${i}.txt | head -4
-      echo "------------------------"
+      echo "----- test${tt}.${i}.txt -----"
+      # Run the test, record output in file testN.MM.out.txt (Run for 10 seconds)
+      timeout 10 ./sample${tt} < test${tt}.${i}.txt > test${tt}.${i}.out.txt
+      if [ $? == "0" ]
+      then      
+        # Add one extra newline at the end of the file (just in case if it is missing)
+        echo >> test${tt}.${i}.out.txt
+        # Ignore case (-i), ignore extra whitespace (-w), ignore blank lines (-B).
+        diff -i -w -B test${tt}.${i}.out.txt expected${tt}.${i}.txt
+        # If the diff command found that two files are the same
+        if [ $? == "0" ]
+        then
+          echo "Test${tt}.${i}: PASS" >> ../GRADES.txt
+        else
+          echo "Test${tt}.${i}: FAIL" >> ../GRADES.txt
+        fi
+      else
+        echo "Test${tt}.${i}: TIMEOUT" >> ../GRADES.txt
+      fi        
     done
-grades=()
-grades+=("QQ1")
-grades+=("QQ2")
-grades+=("QQ3")
-for value in "${grades[@]}"
-  do
-    echo $value
-  done
-
-
-echo 
-
-echo "-----------------------------"
-echo "******** sample2.cpp ********"
-echo "-----------------------------"
-echo "Compiling..."
-make sample2.out
-for i in "${tsts[@]}"
-do
-   echo "----- test2.${i}.txt -----"
-   ./sample2 < test2.${i}.txt > test2.${i}.out.txt
-   diff -i -w -B test2.${i}.out.txt expected2.${i}.txt | head -4
-   echo "------------------------"
+  else 
+    # Executable (sample1, sample2, etc.) was not created by "make" command
+    echo "sample${tt}.cpp did not build"
+    for i in "${tsts[@]}"
+    do
+      echo "Test${tt}.${i}: ERROR" >> ../GRADES.txt
+    done
+  fi
+  echo "------------------------"
+  echo 
 done
-echo 
 
 
-echo "-----------------------------"
-echo "******** sample3.cpp ********"
-echo "-----------------------------"
-echo "Compiling..."
-make sample3.out
-for i in "${tsts[@]}"
-do
-   echo "----- test3.${i}.txt -----"
-   ./sample3 < test3.${i}.txt > test3.${i}.out.txt
-   diff -i -w -B test3.${i}.out.txt expected3.${i}.txt | head -4
-   echo "------------------------"
-done
-echo 
-
-
-echo "-----------------------------"
-echo "******** sample4.cpp ********"
-echo "-----------------------------"
-echo "Compiling..."
-make sample4.out
-for i in "${tsts[@]}"
-do
-   echo "----- test4.${i}.txt -----"
-   ./sample4 < test4.${i}.txt > test4.${i}.out.txt
-   diff -i -w -B test4.${i}.out.txt expected4.${i}.txt | head -4
-   echo "------------------------"
-done
-echo 
-
-
-
-echo "-----------------------------"
-echo "******** sample5.cpp ********"
-echo "-----------------------------"
-echo "Compiling..."
-make sample5.out
-for i in "${tsts[@]}"
-do
-   echo "----- test5.${i}.txt -----"
-   ./sample5 < test5.${i}.txt > test5.${i}.out.txt
-   diff -i -w -B test5.${i}.out.txt expected5.${i}.txt | head -4
-   echo "------------------------"
-done
-echo 
-
-
+#######################################################
+## Run tests in another directory: just sample6.cpp. ##
+#######################################################
 cd ../
 cd lab1-2
 make clean
@@ -106,20 +98,37 @@ echo "-----------------------------"
 echo "******** sample6.cpp ********"
 echo "-----------------------------"
 echo "Compiling..."
-make sample6.out
-for i in "${tsts[@]}"
-do
-   echo "----- test6.${i}.txt -----"
-   ./sample6 < test6.${i}.txt > test6.${i}.out.txt
-   diff -i -w -B test6.${i}.out.txt expected6.${i}.txt | head -4
-   echo "------------------------"
-done
+make sample6
+if [ -f sample6 ]
+then
+  for i in "${tsts[@]}"
+  do
+    echo "----- test6.${i}.txt -----"
+    ./sample6 < test6.${i}.txt > test6.${i}.out.txt
+    grep -i "$(head -1 test6.${i}.out.txt)" expected6.${i}.txt    
+    if [ $? == "0" ]
+    then
+      echo "Test6.${i}: PASS" >> ../GRADES.txt
+    else
+      echo "Test6.${i}: FAIL" >> ../GRADES.txt
+    fi
+  done
+else 
+  # Executable was not created by "make" command
+  echo "sample6.cpp did not build"
+  for i in "${tsts[@]}"
+  do
+    echo "Test6.${i}: ERROR" >> ../GRADES.txt
+  done    
+fi
+echo "------------------------"
 echo 
 
 
+
+
+
 echo "Cleaning up..."
-#make clean
-cd ../
 #sudo rm -fr ds-workspace-username/
 echo
 echo "Done!"
